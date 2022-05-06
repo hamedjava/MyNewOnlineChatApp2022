ec1cd302d35dd03c8f5fb86d2897e3bc4b6fce87
hamedjava <hamedpouyanfar1987@gmail.com>
2022-05-07 02:21:36 +0430

    Design splash Layout and Config splashActivity class timer

--- app/src/main/java/com/example/newonlinechatapp2022/MainActivity.java
+++ app/src/main/java/com/example/newonlinechatapp2022/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.newonlinechatapp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
