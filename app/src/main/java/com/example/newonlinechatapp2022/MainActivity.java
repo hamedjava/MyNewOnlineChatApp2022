068c09726eb08201780b70e0e52400eae6615c01
hamedjava <hamedpouyanfar1987@gmail.com>
2022-05-10 00:35:52 +0430

    nothing changed

--- app/src/main/java/com/example/newonlinechatapp2022/MainActivity.java
+++ app/src/main/java/com/example/newonlinechatapp2022/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
 
         countryCodePicker = (CountryCodePicker)findViewById(R.id.countryCodePicker);
         btn_verify = (Button)findViewById(R.id.btn_main_verify);
