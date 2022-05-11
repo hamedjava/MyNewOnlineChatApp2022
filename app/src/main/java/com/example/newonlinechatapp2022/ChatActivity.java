322b1c66c82928749624985a54d501bc1c6d84c1
hamedjava <hamedpouyanfar1987@gmail.com>
2022-05-11 10:58:45 +0430

    manifest Internt permission added

--- app/src/main/java/com/example/newonlinechatapp2022/ChatActivity.java
+++ app/src/main/java/com/example/newonlinechatapp2022/ChatActivity.java
@@ -1,14 +1,28 @@
 package com.example.newonlinechatapp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.newonlinechatapp2022.PagerAdapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class ChatActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabItem;
+    Toolbar toolbar;
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        tabLayout = findViewById(R.id.tabLayout);
+        toolbar = (Toolbar)findViewById(R.id.chat_toolbar);
     }
 }
