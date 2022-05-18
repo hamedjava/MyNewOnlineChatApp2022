c060b04c5a31d4f885fbf8e798392e1da341ca3f
hamedjava <hamedpouyanfar1987@gmail.com>
2022-05-19 03:01:48 +0430

    add chatview layout and add updateprofileactivity and config profile activity

--- app/src/main/java/com/example/newonlinechatapp2022/ProfileActivity.java
+++ app/src/main/java/com/example/newonlinechatapp2022/ProfileActivity.java
@@ -1,14 +1,110 @@
 package com.example.newonlinechatapp2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    CardView cv_showUserImage;
+    ImageView img_showUserLogo;
+    TextView tv_showUsername;
+    TextView tv_updateUsername;
+    Toolbar toolbar;
+    ImageButton btn_profile_back;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    private String IMAGE_URI_ACCESS_TOKEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setupViews();
+        setSupportActionBar(toolbar);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        btn_profile_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        storageReference = firebaseStorage.getReference();
+        storageReference.child("Image").child(firebaseAuth.getUid()).child("Profile Pic").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                IMAGE_URI_ACCESS_TOKEN = uri.toString();
+                Picasso.get().load(uri).into(img_showUserLogo);
+            }
+        });
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                UserProfile userProfile = snapshot.getValue(UserProfile.class);
+                tv_showUsername.setText(userProfile.getUsername());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Data Not Received..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv_updateUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this,UpdateProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    public void setupViews(){
+        tv_showUsername = findViewById(R.id.tv_showUsername);
+        tv_updateUsername = findViewById(R.id.tv_updateProfile);
+        cv_showUserImage = findViewById(R.id.cv_showUserImage);
+        img_showUserLogo = findViewById(R.id.img_showProfileUser_logo);
+        toolbar = (Toolbar) findViewById(R.id.profile_toolbar);
+        btn_profile_back = (ImageButton) findViewById(R.id.btn_back_viewProfile);
     }
 }
