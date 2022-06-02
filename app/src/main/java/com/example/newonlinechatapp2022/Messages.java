9841c64523bd88c56f49f7bbb1971e54fc0abf28
hamedjava <hamedpouyanfar1987@gmail.com>
2022-06-03 00:37:31 +0430

    create message model

---
+++ app/src/main/java/com/example/newonlinechatapp2022/Messages.java
@@ -0,0 +1,51 @@
+package com.example.newonlinechatapp2022;
+
+public class Messages {
+
+    private String message;
+    private String senderID;
+    private long timeStamp;
+    private String currentTime;
+
+    public Messages(String message, String senderID, long timeStamp, String currentTime) {
+        this.message = message;
+        this.senderID = senderID;
+        this.timeStamp = timeStamp;
+        this.currentTime = currentTime;
+    }
+
+    public Messages() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(String senderID) {
+        this.senderID = senderID;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+}
