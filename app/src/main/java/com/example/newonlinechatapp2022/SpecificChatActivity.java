84d9ae5f16a51b3af1a238f34573de5d1c53e284
hamedjava <hamedpouyanfar1987@gmail.com>
2022-06-03 18:43:19 +0430

    modify specific layout and continue config specific Activity

--- app/src/main/java/com/example/newonlinechatapp2022/SpecificChatActivity.java
+++ app/src/main/java/com/example/newonlinechatapp2022/SpecificChatActivity.java
@@ -3,22 +3,52 @@ package com.example.newonlinechatapp2022;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SpecificChatActivity extends AppCompatActivity {
 
     EditText edt_specific_sendMessage;
     ImageButton btn_send_message
                 ,btn_specific_back;
-    CardView cardView_sendMessage;
+
     Toolbar toolbar;
     ImageView img_userImageViewOnToolbar;
-    TextView tv_specific_username;
+    TextView tv_specific_nameOfUser;
+
+    private String enteredMessage;
+    Intent intent;
+
+    private String receivedMessage
+            ,senderName
+            ,receiverUID
+            ,senderUID;
+
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+
+    String senderRoom,ReceiverRoom;
+    RecyclerView rv_messagesList;
+
+    String currentTime;
+    Calendar calendar;
+    SimpleDateFormat simpleDateFormat;
 
 
     @Override
@@ -27,6 +57,36 @@ public class SpecificChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_specific_chat);
         setupViews();
         setSupportActionBar(toolbar);
+
+        calendar = Calendar.getInstance();
+        simpleDateFormat = new SimpleDateFormat("hh:mm a");
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        btn_specific_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SpecificChatActivity.this, "Toolbar Is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void setupViews(){
@@ -35,5 +95,11 @@ public class SpecificChatActivity extends AppCompatActivity {
         toolbar = (Toolbar)findViewById(R.id.specific_chat_toolbar);
         img_userImageViewOnToolbar = (ImageView)findViewById(R.id.img_specificUserLogo);
         btn_specific_back = (ImageButton)findViewById(R.id.btn_back_specificChat);
+        tv_specific_nameOfUser = (TextView) findViewById(R.id.tv_specific_nameOfUser);
+        intent = getIntent();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
     }
 }
